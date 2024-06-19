0b63d40b4413e8d6d90c1d86243041acad20d6ef
GJC <1581884961@qq.com>
2024-06-19 16:16:53 +0800

    text0

---
+++ tihuan.py
@@ -0,0 +1,11 @@
+import os
+file = r'D:\python\work\0318.txt'
+with open(file, 'r', encoding='utf-8') as f:
+     alllines = f.readlines()  # list形式返回txt的所有行  ['打力矩\n', '查找箱变漏油处\n',...]
+     # print(lines)
+
+with open(file, 'w', encoding='utf-8') as f:
+     for eachline in alllines:
+         eachline = eachline.replace(r'3410', '3420')
+         #eachline = eachline.replace(r'发电机组', '风机')
+         f.write(eachline)
